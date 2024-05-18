A few formula errors fixed

io_handler.py
@@ -5,7 +5,7 @@ import openpyxl
 import pandas as pd
 
 from Logger import Logger
-from __structs__ import LoadCase, LoadsInPlane, LoadsStringers, IO, Parameters
+from __structs__ import LoadCase, LoadsInPlane, LoadsStringers, IO, Parameters, Stringer
 
 logger = Logger('read_excel').logger
 
@@ -25,9 +25,17 @@ def read_config() -> [LoadCase]:
     a = float(config['PARAMETERS']['a'])
     b = float(config['PARAMETERS']['b'])
     t = float(config['PARAMETERS']['t'])
+    stringer_base_w = float(config['PARAMETERS']['stringer_base_w'])
+    stringer_base_t = float(config['PARAMETERS']['stringer_base_t'])
+    stringer_height = float(config['PARAMETERS']['stringer_height'])
+    stringer_neck_width = float(config['PARAMETERS']['stringer_neck_width'])
     main = pd.read_excel(io.output_file, sheet_name='in')
     E = float(main.iat[5, 1])
-    parameters = Parameters(sigma_ul=sigma_ul, sigma_yield=sigma_yield, mu=mu, a=a, b=b, t=t, E=E, sf=sf, sigma_e=E * np.square(np.pi) / (12 * (1 - np.square(mu))) * np.square(t / b))
+    parameters = Parameters(sigma_ul=sigma_ul, sigma_yield=sigma_yield, mu=mu, a=a, b=b, t=t,
+                            stringer_base_w=stringer_base_w, stringer_height=stringer_height,
+                            stringer_base_t=stringer_base_t, stringer_neck_width=stringer_neck_width,
+                            E=E, sf=sf,
+                            sigma_e=E * np.square(np.pi) / (12 * (1 - np.square(mu))) * np.square(t / b))
     return read_excel(in_plane_stresses, axial_stringer_stresses, params=parameters), io, parameters
 
 
@@ -42,7 +50,8 @@ def read_excel(in_plane_stresses: str, axial_stringer_stresses: str, params: Par
     for i, load_case in enumerate(load_cases):
         while ips.iat[k, 2] == i + 1:
             load_case.LoadsInPlane.append(
-                LoadsInPlane(e_id=ips.iat[k, 0], xx=params.sf * ips.iat[k, 5], yy=params.sf * ips.iat[k, 7], xy=params.sf * ips.iat[k, 6],
+                LoadsInPlane(e_id=ips.iat[k, 0], xx=params.sf * ips.iat[k, 5], yy=params.sf * ips.iat[k, 7],
+                             xy=params.sf * ips.iat[k, 6],
                              von_Mises=params.sf * ips.iat[k, 8],
                              reserve_factor=np.abs(params.sigma_ul / (params.sf * float(ips.iat[k, 8])))))
             k += 1
@@ -50,7 +59,8 @@ def read_excel(in_plane_stresses: str, axial_stringer_stresses: str, params: Par
                 break
         while ass.iat[j, 2] == i + 1:
             load_case.LoadsStringers.append(LoadsStringers(e_id=ass.iat[j, 0], stress=params.sf * ass.iat[j, 4],
-                                                           reserve_factor=np.abs(params.sigma_ul / (params.sf * ass.iat[j, 4]))))
+                                                           reserve_factor=np.abs(
+                                                               params.sigma_ul / (params.sf * ass.iat[j, 4]))))
             j += 1
             if j > len(ass) - 1:
                 break
@@ -106,6 +116,7 @@ def parse_stringer_analysis(ws, load_cases):
     column = 2
     for i, load_case in enumerate(load_cases):
         row = 73
+        row_geo = 80
         for k, stringer in enumerate(load_case.Stringers):
             cellref = ws.cell(row=row + k, column=column)
             cellref.value = stringer.sigma_axial
@@ -113,4 +124,18 @@ def parse_stringer_analysis(ws, load_cases):
             cellref.value = stringer.sigma_crip
             cellref = ws.cell(row=row + k, column=column + 2)
             cellref.value = stringer.reserve_factor
+            parse_geometric_properties(ws, stringer, row_geo + k)
         column += 5
+
+
+def parse_geometric_properties(ws, stringer: Stringer, row):
+    column = 2
+    for k, geo in enumerate(stringer.geometrics):
+        cellref = ws.cell(row=row, column=column)
+        cellref.value = geo.I
+        cellref = ws.cell(row=row, column=column + 1)
+        cellref.value = geo.r_gyr
+        cellref = ws.cell(row=row, column=column + 2)
+        cellref.value = geo.lamda
+        cellref = ws.cell(row=row, column=column + 3)
+        cellref.value = geo.lamda_crit

main.py
@@ -3,7 +3,7 @@ import configparser
 import numpy as np
 
 from Logger import Logger
-from __structs__ import LoadCase, Parameters, Panel, LoadsInPlane, LoadsStringers, Stringer, IO
+from __structs__ import LoadCase, Parameters, Panel, LoadsInPlane, LoadsStringers, Stringer, IO, Geometric
 from io_handler import read_config, parse_excel
 
 logger = Logger('main').logger
@@ -21,7 +21,7 @@ logger = Logger('main').logger
 
 
 def calc_buckling_factors(params: Parameters, sigma_y: float, sigma_x: float) -> [float,
-                                                                                                                 float]:
+                                                                                  float]:
     """Function to minimize the stress and calculate the number of halve waves in x- and y-direction"""
     alpha = params.a / params.b
     beta = sigma_y / sigma_x
@@ -30,7 +30,7 @@ def calc_buckling_factors(params: Parameters, sigma_y: float, sigma_x: float) ->
     for m in range(1, 10):
         for n in range(1, 10):
             k_sigma = np.square(np.square(m) + np.square(n * alpha)) / (
-                        np.square(alpha) * (np.square(m) + beta * np.square(n * alpha)))
+                    np.square(alpha) * (np.square(m) + beta * np.square(n * alpha)))
             sigma_crit = k_sigma * params.sigma_e
             if np.abs(sigma_crit) < np.abs(min_sigma) or min_sigma == -1:
                 min_sigma = sigma_crit
@@ -74,7 +74,7 @@ def avg_stringer(stringer: LoadsStringers):
     return numerator / len(stringer)
 
 
-def calc_avg_sigma_combined(load_cases: [LoadCase], E):
+def calc_avg_sigma_combined(load_cases: [LoadCase], params: Parameters):
     for load_case in load_cases:
         for i in range(4):
             stringer = load_case.LoadsStringers[i * 3:i * 3 + 3]
@@ -82,15 +82,59 @@ def calc_avg_sigma_combined(load_cases: [LoadCase], E):
             avg_stringer_xx = avg_stringer(stringer) * 3 * 56800
             avg_sigma_panel = avg_panel(panel)[0] * 6 * 160000
             sigma_combined = (avg_stringer_xx + avg_sigma_panel) / (56800 * 3 + 160000 * 6)
-            sigma_crip = sigma_combined - 1 / E * np.square(sigma_combined / (2 * np.pi)) * np.square(115.4074149)
+            # sigma_crip = params.sigma_yield - 1 / params.E * np.square(params.sigma_yield / (2 * np.pi)) * np.square(
+            #     115.4074149)
+            # b_1_1 = params.stringer_base_w / 2 - (params.stringer_neck_width / 2) * (
+            #             0.25 * params.stringer_neck_width / params.stringer_base_t)
+            b_1_2 = params.stringer_height - params.stringer_base_t / 2 * (
+                        2 - 0.5 * params.stringer_neck_width / params.stringer_base_t)
+            # x_1 = b_1_1 / params.stringer_base_t * np.sqrt(params.sigma_yield / (0.41 * params.E))
+            x_2 = b_1_2 / params.stringer_neck_width * np.sqrt(params.sigma_yield / (0.41 * params.E))
+            if x_2 <= 1.095:
+                alpha_2 = 1.4 - 0.628 * x_2
+            elif x_2 <= 1.633:
+                alpha_2 = 0.78 / x_2
+            else:
+                alpha_2 = 0.69 / np.power(x_2, 0.75)
+            sigma_crip = alpha_2 * params.sigma_yield
+            if params.sigma_yield < sigma_crip:
+                R_f = params.sigma_yield / sigma_combined
+            else:
+                R_f = sigma_crip / sigma_combined
             load_case.Stringers.append(
-                Stringer(sigma_axial=sigma_combined, sigma_crip=sigma_crip, reserve_factor=sigma_crip / sigma_combined))
+                Stringer(sigma_axial=sigma_combined, sigma_crip=sigma_crip, reserve_factor=np.abs(R_f)))
+
+
+def calc_lamda(params: Parameters, load_cases: [LoadCase]):
+    A_skin = params.t * params.b
+    A_stringer_base = params.stringer_base_w * params.stringer_base_t
+    A_stringer_neck = (params.stringer_height - params.stringer_base_t) * params.stringer_neck_width
+    A = A_skin + A_stringer_base + A_stringer_neck
+    z_bar_numerator = -params.t / 2 * A_skin + params.stringer_base_t / 2 * A_stringer_base + (
+            params.stringer_base_t + (params.stringer_height - params.stringer_base_t) / 2) * A_stringer_neck
+    z_bar = z_bar_numerator / A
+    I_skin = np.power(params.t, 3) * params.b / 12 + A_skin * np.square(-params.t / 2 - z_bar)
+    I_stringer_base = np.power(params.stringer_base_t, 3) * params.stringer_height / 12 + A_stringer_base * np.square(
+        params.t / 2 - z_bar)
+    I_stringer_neck = np.power(params.stringer_height - params.stringer_base_t,
+                               3) * params.stringer_neck_width / 12 + A_stringer_neck * np.square(
+        params.stringer_base_t + (params.stringer_height - params.stringer_base_t) / 2 - z_bar)
+    I = I_skin + I_stringer_base + I_stringer_neck
+    r_gyr = np.sqrt(I / A)
+    lamda = params.a / r_gyr
+    for load_case in load_cases:
+        for stringer in load_case.Stringers:
+            if np.abs(stringer.sigma_crip) < np.abs(params.sigma_yield):
+                lamda_crit = np.sqrt(2 * np.square(np.pi) * params.E / np.abs(stringer.sigma_crip))
+            else:
+                lamda_crit = np.sqrt(2 * np.square(np.pi) * params.E / params.sigma_yield)
+            stringer.geometrics.append(Geometric(I=I, r_gyr=r_gyr, lamda=lamda, lamda_crit=lamda_crit))
 
 
 if __name__ == '__main__':
     load_cases, io, parameters = read_config()
-    # parameters = get_parameters()
     calc_avg_sigma_panel(load_cases, parameters)
-    calc_avg_sigma_combined(load_cases, 64744.31)
+    calc_avg_sigma_combined(load_cases, parameters)
+    calc_lamda(parameters, load_cases)
     parse_excel(io, load_cases)
     pass